a8ea2ff4e2135ac3c9d2075b6f17ba718e0072f4
Aseem Ratha <aseemratha@gmail.com>
2024-10-04 17:10:09 -0400

    TRIANGLE!!!

--- first_app.h
+++ first_app.h
@@ -1,8 +1,12 @@
 #pragma once
 
-#include "lve_window.h"
-#include "lve_pipeline.h"
 #include "lve_device.h"
+#include "lve_pipeline.h"
+#include "lve_swap_chain.h"
+#include "lve_window.h"
+
+#include <memory>
+#include <vector>
 
 namespace lve{
     class FirstApp {
@@ -12,17 +16,26 @@ namespace lve{
           static constexpr int WIDTH  = 800;
           static constexpr int HEIGHT = 600;
 
+          FirstApp();
+          ~FirstApp();
+
+          FirstApp(const FirstApp &) = delete;
+          FirstApp &operator=(const FirstApp &) = delete;
+
           void run();
 
         private:
 
+          void createPipelineLayout();
+          void createPipeline();
+          void createCommandBuffers();
+          void drawFrame();
+
           LveWindow lveWindow{WIDTH, HEIGHT, "Hellow Vulkan!"};
           LveDevice lveDevice{lveWindow};
-          LvePipeline lvePipeline{
-              lveDevice,
-              "shaders/simple_shader.vert.spv",
-              "shaders/simple_shader.frag.spv",
-              LvePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)
-          };
+          LveSwapChain lveSwapChain{lveDevice, lveWindow.getExtent()};
+          std::unique_ptr<LvePipeline> lvePipeline;
+          VkPipelineLayout pipelineLayout;
+          std::vector<VkCommandBuffer> commandBuffers;
     };
 }
